14b3509d9092d5de4e991f7a4a46496fbe5188fc
13650964250@163.com <nageshui071011>
2019-08-14 10:19:04 +0800

    11

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="app-main">
     <transition name="fade-transform" mode="out-in">
-     <!-- <router-view :key="key" />-->
+      <!-- <router-view :key="key" />-->
       <keep-alive :include="cachedViews">
-        <router-view></router-view>
+        <router-view />
       </keep-alive>
     </transition>
   </section>
--- src/store/getters.js
+++ src/store/getters.js
@@ -7,6 +7,6 @@ const getters = {
   roles: state => state.user.roles,
   permission_routes: state => state.permission.routes,
   visitedViews: state => state.tagsView.visitedViews,
-  cachedViews: state => state.tagsView.cachedViews,
+  cachedViews: state => state.tagsView.cachedViews
 }
 export default getters
